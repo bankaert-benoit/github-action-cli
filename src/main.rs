af93ec66c65c7c6cd69f841e634d68ce44a8db5c
Bankaert Benoit <bankaert.benoit@gmail.com>
2025-08-18 13:38:33 +0200

    feat: setup the first commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use clap::{ Parser, Subcommand };
+
+/// TODO: Write documentation
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// List the workflows in the given directory
+    List {
+        #[arg(short, long, default_value_t = String::from("."))]
+        path: String,
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    match &cli.command {
+        Some(Commands::List { path }) => {
+            println!("Running list on path: {}", path)
+        },
+        None => {}
+    }
+
 }
